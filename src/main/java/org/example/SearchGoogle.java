a4afee80710ee650bcc3c999f37369f69f726709
tarawithoutbudimann <tamarasashikirana@mail.ugm.ac.id>
2024-06-27 00:40:18 +0700

    first commit

---
+++ src/main/java/org/example/SearchGoogle.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class SearchGoogle {
+    WebDriver driver;
+    @Given("I Open browser")
+    public void iOpenBrowser() {
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
+        System.setProperty("webdriver.chrome.driver", dir+"/driver/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @And("Open website Google")
+    public void openWebsiteGoogle() throws InterruptedException {
+        driver.get("https://www.google.co.id/");
+        Thread.sleep(1000);
+    }
+
+    @And("Located on google website")
+    public void locatedOnGoogleWebsite() {
+        driver.findElement(By.name("btnK")).isDisplayed();
+    }
+
+    @When("I search {string}")
+    public void iSearch(String searchValue) {
+        driver.findElement(By.name("q")).sendKeys(searchValue);
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+    }
+
+    @Then("Showing result related with {string}")
+    public void showingResultRelatedWith(String result) {
+        driver.findElement(By.xpath("//*[@id='rso']/div[1]/div/div/div[1]/div/div/span/a/h3")).isDisplayed();
+        driver.close();
+        driver.quit();
+    }
+}
